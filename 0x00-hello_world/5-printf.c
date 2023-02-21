3dcff0081c4a5a4b298d5c3fb6686b421162f0b2
My name <Myemail@gmail.com>
2023-02-21 07:37:54 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,28 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- *
- * Return: Always 0 (Success)
+ * main - main block
+ * Return: 0
  */
 int main(void)
 {
-        printf("with proper prammar, but the outcome is a piece of art,\n");
-        return (0);
-}#include<stdio.h>
-/**
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-        printf("with proper prammar, but the outcome is a piece of art,\n");
-        return (0);
-}#include<stdio.h>
-/**
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-        printf("with proper prammar, but the outcome is a piece of art,\n");
-        return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
