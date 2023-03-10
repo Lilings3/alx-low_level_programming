81f3304704b555dee0ea7930562409388789089e
My name <Myemail@gmail.com>
2023-03-10 14:58:52 -0800

    1. Silence is argument carried out by other means

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,35 +1,14 @@
 #include <stdio.h>
-		
-#include "main.h"
-		
 
-		
 /**
-		
- * main - print the number of arguments passed to the program
-		
- * @argc: number of arguments
-		
- * @argv: array of arguments
-		
- *
-		
- * Return: Always 0 (Success)
-		
+ * main - prints its name, followed by a new line
+ * @argc: n args
+ * @argv: arr args
+ * Return: 0
  */
-		
-
-		
-int main(int argc, char *argv[])
-		
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-		
-(void) argv; /*Ignore argv*/
-		
-	printf("%d\n", argc - 1);
-		
-
-		
+	if (argc > 0)
+		printf("%d\n", argc - 1);
 	return (0);
-		
 }
