5c92370a797035597b1f39ea407331c2716a90f3
Lilings3 <nkosi.sabelo.520@gmail.com>
2023-04-07 08:58:51 -0700

    Task0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,18 +8,26 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int i;
-	unsigned int dec_val = 0;
+	unsigned int total = 0;
+	int len, i;
+	int decval = 1;
 
 	if (!b)
 		return (0);
 
-	for (i = 0; b[i]; i++)
+	for (len = 0; b[len] != '\0'; len++)
+		;
+
+	i = (len - 1);
+	for (; i >= 0; i--)
 	{
-		if (b[i] < '0' || b[i] > '1')
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		dec_val = 2 * dec_val + (b[i] - '0');
+
+		if (b[i] == '1')
+			total += decval;
+		decval *= 2;
 	}
 
-	return (dec_val);
+	return (total);
 }
